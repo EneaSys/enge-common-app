2c344902c9d293b8dd54475c352fe51231029620
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-01-03 12:10:08 +0100

    add import

--- src/lib/common-app.module.ts
+++ src/lib/common-app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { StalPaginatorModule } from '@stal/paginator';
 
 @NgModule({
 	imports: [
 		/* add this import only in app module
 		StalEventerModule
 		*/
+		StalPaginatorModule,
 	],
 	providers: [
 
