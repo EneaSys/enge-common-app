6192fe8bbe97fafa847c3b9f4e9e838eb30bd6ba
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-01-03 11:40:26 +0100

    fix

--- src/lib/common-app.module.ts
+++ src/lib/common-app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
 	imports: [
-
+		/* add this import only in app module
+		StalEventerModule
+		*/
 	],
 	providers: [
-
-	],
-	exports: [
 
 	],
 })
