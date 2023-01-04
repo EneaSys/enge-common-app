aebd5caebf5dd32ac81188b93c3516cb6a2717d8
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-01-04 15:28:32 +0100

    add dependency

--- src/lib/common-app.module.ts
+++ src/lib/common-app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StalPaginatorModule } from '@stal/paginator';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
 	imports: [
@@ -14,6 +15,7 @@ import { StalPaginatorModule } from '@stal/paginator';
 	exports: [
 		StalPaginatorModule,
 		CommonModule,
+		ButtonModule, // <-- Used in list page for add button
 	]
 })
 export class EngeCommonAppModule { }
